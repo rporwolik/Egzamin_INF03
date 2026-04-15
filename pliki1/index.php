e6a479a36855d345168c315a83d19d205d31e52b
rporwolik <robporwoll44@gmail.com>
2026-04-15 16:46:31 +0200

    Lorem ipsum sit amet

---
+++ pliki1/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konfigurator samochodów</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <section id="container">
+        <header>
+            <h1>Serwis konfiguracji samochodów</h1>
+        </header>
+        <nav>
+            <h2>Samochody</h2>
+            <h2>Konfigurator</h2>
+            <h2>Kontakt</h2>
+        </nav>
+        <main>
+            <section id="lewa">
+                <table>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </section>
+            <section id="srodek">
+
+            </section>
+            <section id="prawa">
+                <h3>111 222 444</h3>
+                <img src="a3.png" alt="Samochód">
+            </section>
+        </main>
+        <footer>
+            <p>Stronę wykonał: 0000000000</p>
+        </footer>
+    </section>
+    
+</body>
+</html>
